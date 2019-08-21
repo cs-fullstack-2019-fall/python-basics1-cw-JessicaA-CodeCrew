0fc1975166fc6f98a97cd0873d61abae336ecd20
autumn-ragland <raglanda998@gmail.com>
2019-08-21 14:13:05 -0500

    Graded 08-21-2019 14:11:46

--- 0821PYCW.py
+++ 0821PYCW.py
@@ -1,26 +1,26 @@
 # Create two variables. One should equal “My name is: “ and the other should equal your actual name. Print the two variables in one print message.
 
-# v1 = "My name is: "
-# v2 = "Jessica"
-# print(v1 + v2)
+v1 = "My name is: "
+v2 = "Jessica"
+print(v1 + v2)
 
 # Problem 2:
 # Ask the user to enter the extra credit they earned. If they entered less than 5 print “That’s not enough extra credit.” If they entered more than 20 print “That’s too much extra credit”.
 
-# userGrade = int(input("How much extra credit did you earn?"))
-# if userGrade < 5 :
-#     print("That's not enough extra credit")
-# elif userGrade > 20:
-#     print("That's too much extra credit")
+userGrade = int(input("How much extra credit did you earn?"))
+if userGrade < 5 :
+    print("That's not enough extra credit")
+elif userGrade > 20:
+    print("That's too much extra credit")
 
 # Ask a user to enter a password. Enter a password. Ask user to reenter password. Check to see if they are correct.
 
-# userInput1 = input("Enter a password: ")
-# userInput2 = input("Reenter a password: ")
-# if userInput1 == userInput2:
-#     print("Correct")
-# else:
-#     print("Incorrect")
+userInput1 = input("Enter a password: ")
+userInput2 = input("Reenter a password: ")
+if userInput1 == userInput2:
+    print("Correct")
+else:
+    print("Incorrect")
 
 # Ask for two card numbers. If it equals 21 print BLACKJACK!, if it’s greater than 21 print BUST!, if it’s less than 21 print “The total is [THE TOTAL]”
 
